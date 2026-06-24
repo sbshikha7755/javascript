c85a8f8c29ee1516fab70e43553d95c5847f4f32
sbshikha7755 <sbshikha7755@gmail.com>
2026-06-24 15:31:49 +0530

    function

---
+++ Strings.js
@@ -0,0 +1,48 @@
+// let  arr ="Ram";
+// console.log(arr);
+
+
+// let  arr ='Rama';
+// console.log(arr);
+//  let amit=arr.length;
+//  console.log(amit);
+
+
+//  let a="this is pen"
+//  let len= a.length;
+//  console.log(len);
+
+// let a = "This is pen";
+// let document=  document.getElementById("len").innerHTML=a.length;
+
+// let a = "This is pen";
+//  document.getElementById("name").innerHTML= a;
+
+//   document.getElementById("len").innerHTML=a.length;
+
+//   let head= "Shikhu ji";
+//   document.getElementById("head").innerHTML= head;
+  
+
+let header = "Template Strings";
+let tags = ["template strings", "javascript", "es6"];
+let len = tags.length;
+// console.log(len);
+
+ document.getElementById("head").innerHTML= header;
+//   document.getElementById("name").innerHTML= tags;
+
+  for(let i =0 ; i<len; i++){
+    // console.log(i);
+    // console.log(tags);
+    document.getElementById("name").innerHTML= tags;
+  }
+
+ for(let i =0 ; i<tags.length; i++){
+    // console.log(i);
+    // console.log(tags);
+    // document.getElementById("name").innerHTML= tags;
+  }
+
+
+ 
---
+++ function.js
@@ -0,0 +1,40 @@
+// function program........
+
+// function add ( a,  b){
+//     let c ;
+//     c = a + b;
+//     return c;
+
+// }
+// let sum = add (10 , 5);
+// console.log(sum);
+
+// let sum1 = add (40 , 20);;
+// console.log(sum1);
+// document.getElementById("para").innerHTML=sum;
+
+function variab ( a,  b){
+   return a*b;
+
+} 
+// let multi = variable (10 , 5); 
+// console.log(multi);
+// document.getElementById("para").innerHTML=multi +" and "+sum
+// document.getElementById("para").innerHTML=sum.multi
+// document.getElementById("para").innerHTML=multi ;
+
+
+// // console.log(variab(10,5));
+console.log(variab(20,10))
+// document.getElementById("para").innerHTML=variab(6,9);
+variab()
+console.log()
+
+function ram ( a , b){
+   
+      
+}
+console.log(a>b)
+
+
+
